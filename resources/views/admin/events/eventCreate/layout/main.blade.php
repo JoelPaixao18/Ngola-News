72a222a7fb96f3dd64398df768cf579a4331303b
unknown <Administrador@Desenvolvedor10.infosi.local>
2025-08-08 10:08:41 +0100

    baixei as novas alterações

--- resources/views/admin/events/eventCreate/layout/main.blade.php
+++ resources/views/admin/events/eventCreate/layout/main.blade.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">1
+<html lang="zxx">
+
+
+<!-- Mirrored from bestwpware.com/html/tf/duralux-demo/leads.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 21 Jul 2025 12:20:41 GMT -->
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="IE=edge">
@@ -12,53 +16,53 @@
     <title>@yield('title')</title>
     <!--! END:  Apps Title-->
     <!--! BEGIN: Favicon-->
-    <link rel="shortcut icon" type="image/x-icon" href="{{url('assets/images/favicon.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ url('assets/images/favicon.png') }}">
     <!--! END: Favicon-->
     <!--! BEGIN: Bootstrap CSS-->
-    <link rel="stylesheet" type="text/css" href="{{url('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/bootstrap.min.css') }}">
     <!--! END: Bootstrap CSS-->
     <!--! BEGIN: Vendors CSS-->
-    <link rel="stylesheet" type="text/css" href="{{url('assets/vendors/css/vendors.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('assets/vendors/css/select2.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{url('assets/vendors/css/select2-theme.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/vendors/css/vendors.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/vendors/css/dataTables.bs5.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/vendors/css/select2.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/vendors/css/select2-theme.min.css') }}">
     <!--! END: Vendors CSS-->
     <!--! BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{url('assets/css/theme.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/theme.min.css') }}">
     <!--! END: Custom CSS-->
     <!--! HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries !-->
     <!--! WARNING: Respond.js doesn"t work if you view the page via file: !-->
     <!--[if lt IE 9]>
-			<script src="https:oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-			<script src="https:oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+   <script src="https:oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+   <script src="https:oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
+
 <body>
-@include('admin.events.eventCreate.parcial.header')
-@include('admin.events.eventCreate.parcial.menu')
-@include('admin.events.eventCreate.parcial.theme')
-
-<div>
-    @if(session('msg'))
-        <p class="msg" style="background-color=green; color=red">{{session('msg')}}</p>
-     @endif
-    @yield('container-eventCreate')
-</div>
-
-@include('admin.events.eventCreate.parcial.footer')
+    @include('admin.events.eventCreate.parcial.header')
+    @include('admin.events.eventCreate.parcial.menu')
+    <main class="nxl-container">
+        @yield('content-eventCreate')
+        @include('admin.events.eventCreate.parcial.footer')
+    </main>
     <!--! BEGIN: Vendors JS !-->
-    <script src="{{url('assets/vendors/js/vendors.min.js')}}"></script>
+    <script src="{{ url('assets/vendors/js/vendors.min.js') }}"></script>
     <!-- vendors.min.js {always must need to be top} -->
-    <script src="{{url('assets/vendors/js/apexcharts.min.js')}}"></script>
-    <script src="{{url('assets/vendors/js/circle-progress.min.js')}}"></script>
-    <script src="{{url('assets/vendors/js/select2.min.js')}}"></script>
-    <script src="{{url('assets/vendors/js/select2-active.min.js')}}"></script>
+    <script src="{{ url('assets/vendors/js/dataTables.min.js') }}"></script>
+    <script src="{{ url('assets/vendors/js/dataTables.bs5.min.js') }}"></script>
+    <script src="{{ url('assets/vendors/js/select2.min.js') }}"></script>
+    <script src="{{ url('assets/vendors/js/select2-active.min.js') }}"></script>
     <!--! END: Vendors JS !-->
     <!--! BEGIN: Apps Init  !-->
-    <script src="{{url('assets/js/common-init.min.js')}}"></script>
-    <script src="{{url('assets/js/reports-leads-init.min.js')}}"></script>
+    <script src="{{ url('assets/js/common-init.min.js') }}"></script>
+    <script src="{{ url('assets/js/leads-init.min.js') }}"></script>
     <!--! END: Apps Init !-->
     <!--! BEGIN: Theme Customizer  !-->
-    <script src="{{url('assets/js/theme-customizer-init.min.js')}}"></script>
+    <script src="{{ url('assets/js/theme-customizer-init.min.js') }}"></script>
     <!--! END: Theme Customizer !-->
 </body>
+
+
+<!-- Mirrored from bestwpware.com/html/tf/duralux-demo/leads.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 21 Jul 2025 12:20:42 GMT -->
+
 </html>
