73858addc131f166a0d714117e844ba307a26cd9
unknown <Administrador@Desenvolvedor10.infosi.local>
2025-08-12 14:57:16 +0100

    operando

--- resources/views/admin/authors/author/index.blade.php
+++ resources/views/admin/authors/author/index.blade.php
@@ -221,7 +221,7 @@
                     <div>{{ session('success') }}</div>
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-            @endif --}}
+            @endif 
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card stretch stretch-full">
