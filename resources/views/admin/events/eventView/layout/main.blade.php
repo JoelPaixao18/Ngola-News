b792a51fc163802db18adfdbc5712151325b34e5
unknown <Administrador@Desenvolvedor10.infosi.local>
2025-08-12 14:31:48 +0100

    atualizações BLM

--- resources/views/admin/events/eventView/layout/main.blade.php
+++ resources/views/admin/events/eventView/layout/main.blade.php
@@ -36,6 +36,13 @@
    <script src="https:oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
    <script src="https:oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
+  <style>
+    .img-fluid {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+}
+  </style>
 </head>
 
 <body>
